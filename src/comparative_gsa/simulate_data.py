76ebcab7de5bc58b9696d1f5e5e9a803907fae2b
Levan Bokeria <levan.bokeria@gmail.com>
2025-08-05 17:03:52 +0100

    adding a module to simulate data and save boolean indices

---
+++ src/comparative_gsa/simulate_data.py
@@ -0,0 +1,54 @@
+# Import relevant libraries
+import os
+from ModularCirc import BatchRunner
+import numpy as np
+import pandas as pd
+from ModularCirc.Models.NaghaviModel import NaghaviModel, NaghaviModelParameters, TEMPLATE_TIME_SETUP_DICT
+from SALib.analyze.sobol import analyze
+from SALib.sample.sobol import sample as sobol_sample
+from SALib.analyze.sobol import analyze as sobol_analyze
+import matplotlib.pyplot as plt
+from autoemulate.compare import AutoEmulate
+from SALib.analyze.sobol import analyze
+import numpy as np
+import json
+
+def simulate_data(batch_runner, simulation_out_path, n_jobs=8):
+    """
+    Runs a batch of simulations using the provided BatchRunner instance and saves the results.
+
+    Args:
+        batch_runner (BatchRunner): An instance of BatchRunner configured with samples to simulate.
+        simulation_out_path (str): Directory path where simulation results and metadata will be saved.
+        n_jobs (int, optional): Number of parallel jobs to use for running simulations. Defaults to 8.
+
+    Returns:
+        tuple:
+            - simulations (list): List of simulation results, where each entry corresponds to a sample.
+            - bool_indices (list): List of indices in `simulations` where the result is a boolean (indicating failure).
+
+    Side Effects:
+        - Saves raw simulation results to a file in `simulation_out_path`.
+        - Saves indices of failed simulations (boolean results) to `bool_indices.json` in `simulation_out_path`.
+    """
+
+    # Get the number of samples from the batch runner
+    n_samples = batch_runner._samples.shape[0]
+
+    print(f"Running batch simulation with {n_samples} samples...")
+    simulations = batch_runner.run_batch(
+            n_jobs=n_jobs,
+            output_path=os.path.join(simulation_out_path, f'raw_simulations_{n_samples}_samples')
+        )
+
+    # Track the boolean indices of failed simulations
+    bool_indices = [index for index, value in enumerate(simulations) if isinstance(value, bool)]
+
+    # Save the boolean indices to a JSON file
+    with open(os.path.join(simulation_out_path,"bool_indices.json"), 'w') as f:
+        json.dump(bool_indices, f)
+
+    # Print the boolean indices
+    print(bool_indices)
+
+    return simulations, bool_indices
